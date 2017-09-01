eed72d26dacbf25a6852da1b3f106152eca9af8a
jeremylenz <jeremylenz@gmail.com>
2017-09-01 18:40:51 -0400

    New Report Form persists to database

--- src/components/NewReportForm.js
+++ src/components/NewReportForm.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import config from '../config.js'
-import { Form, Grid, Dropdown, Header } from 'semantic-ui-react'
+import { Form, Grid, Dropdown, Header, TextArea, Divider, Button, Link } from 'semantic-ui-react'
 
+const OUR_API_URL = config.OUR_API_URL
 
 class NewReportForm extends React.Component {
 
   constructor() {
     super ()
 
-    let typeOptions = ['']
+    let reportTypes = [
+      'Safety issue - General',
+      'Police vehicle in bike lane',
+      'Other motor vehicle in a bike lane',
+      'Police activity',
+      'Street defect - Pothole/Pavement issue',
+      'Obstruction in a bike lane',
+      'Police blockade',
+      'Standing water on bike path',
+      'Eyes on the Street / Improvement',
+      'Just saying hi'
+    ]
+
+    let typeOptions = reportTypes.map((rt, idx) => {
+      return {
+        key: idx,
+        value: rt,
+        text: rt
+      }
+    })
+
 
     this.state = {
       bikePathsLoaded: false,
@@ -16,33 +37,13 @@ class NewReportForm extends React.Component {
       bikePaths: [],
       locations: [],
       bikePathOptions: [],
-      locationOptions: []
+      locationOptions: [],
+      typeOptions: typeOptions
 
     }
   }
 
-  render () {
-    return (
-      <div className="put-it-in-a-div">
-        <Grid centered columns={2}>
-          <Grid.Column>
-            <Form>
-              <Header as='h3'>New Report</Header>
-              <label htmlFor='bikepath'>Bike path:</label>
-              <Form.Dropdown placeholder='Bike path: ' id='bikepath' options={this.state.bikePathOptions} />
-              <label htmlFor='location'>Location:</label>
-              <Form.Dropdown placeholder='Location: ' id='location' options={this.state.locationOptions} />
 
-
-            </Form>
-          </Grid.Column>
-        </Grid>
-
-
-
-      </div>
-    )
-  }
 
   componentDidMount() {
     fetch(`${config.OUR_API_URL}/bike_paths`)
@@ -79,6 +80,95 @@ class NewReportForm extends React.Component {
     })
   }
 
+  handleSubmit = (event) => {
+
+    let reportType = event.target.parentElement.children[3].children[0].innerText
+    let bikePath = event.target.parentElement.children[6].children[0].innerText
+    let location = event.target.parentElement.children[9].children[0].innerText
+    let details = this.state.details
+    let bikePathId = this.state.bikePaths.find((bp) => {return bp.name === bikePath}).id
+    let locationId = this.state.locations.find((loc) => {return loc.name === location}).id
+    let userId = 1
+
+    this.saveReport(reportType, details, bikePathId, locationId, userId)
+
+  }
+
+  saveReport = (reportType, details, bikePathId, locationId, userId) => {
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json')
+    myHeaders.append('Accept', 'application/json')
+
+    let myBody =
+    {"report": {
+                  "report_type": reportType,
+                  "details": details,
+                  "likes": 0
+                  },
+      "bike_path": {
+                  "id": bikePathId
+                },
+      "location": {
+                  "id": locationId
+                },
+      "user": {
+                  "id": userId
+                  }
+    }
+
+    fetch(`${OUR_API_URL}/reports`,
+      {method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(myBody)
+    })
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+
+    }
+
+
+  handleTextAreaChange = (event) => {
+    this.setState({
+      details: event.target.value
+    })
+  }
+
+
+
+
+render () {
+  return (
+    <div className="put-it-in-a-div">
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <Form>
+            <Header as='h2'>New Report</Header>
+            <Header as='h3'>What are you reporting?</Header>
+
+            <label htmlFor='type'>Report type:</label>
+            <Form.Dropdown placeholder='Report type:' id='type' options={this.state.typeOptions} onChange={this.handleTypeDDChange}/>
+
+            <Header as='h3'>Where did you see it?</Header>
+            <label htmlFor='bikepath'>Bike path:</label>
+            <Form.Dropdown placeholder='Bike path: ' id='bikepath' options={this.state.bikePathOptions} />
+            <label htmlFor='location'>Location:</label>
+            <a href='/newlocation' style={{float: 'right'}}>New location</a>
+            <Form.Dropdown placeholder='Location: ' id='location' options={this.state.locationOptions} />
+
+            <Header as='h3'>Details</Header>
+            <TextArea autoHeight placeholder='Give us the deets' rows={2} value={this.state.details} onChange={this.handleTextAreaChange}/>
+            <Divider />
+            <Button type='submit' onClick={this.handleSubmit}>Submit</Button>
+
+          </Form>
+        </Grid.Column>
+      </Grid>
+
+
+
+    </div>
+  )
+}
 
 }
 
